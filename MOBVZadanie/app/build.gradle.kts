f0b829fbab4df46ebba007a896e756ccec58ce15
Initial commit <xsefcikm@stuba.sk>
2023-10-10 11:57:08 +0200

    style: add basic styles and english language

--- MOBVZadanie/app/build.gradle.kts
+++ MOBVZadanie/app/build.gradle.kts
@@ -44,5 +44,6 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.mapbox.maps:android:10.16.0") // Použite najnovšiu verziu, ak je k dispozícii.
+    implementation("com.google.android.material:material:1.9.0")
 
 }

3441368a56d0a0b79cce5a27013d0e8d133a5f40
Initial commit <xsefcikm@stuba.sk>
2023-12-03 17:01:45 +0100

    feat: feed list

--- MOBVZadanie/app/src/main/java/eu/mcomputing/mobv/zadanie/adapters/FeedAdapter.kt
+++ MOBVZadanie/app/src/main/java/eu/mcomputing/mobv/zadanie/adapters/FeedAdapter.kt
@@ -1,5 +1,8 @@
 package eu.mcomputing.mobv.zadanie.adapters
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +13,9 @@ import androidx.recyclerview.widget.RecyclerView
 import eu.mcomputing.mobv.zadanie.utils.MyItemDiffCallback
 import eu.mcomputing.mobv.zadanie.R
 import eu.mcomputing.mobv.zadanie.data.db.entities.UserEntity
+import java.util.concurrent.Executors
+import android.os.Handler
+import android.os.Looper
 
 class FeedAdapter : RecyclerView.Adapter<FeedAdapter.FeedViewHolder>() {
     private var items: List<UserEntity> = listOf()
@@ -27,6 +33,48 @@ class FeedAdapter : RecyclerView.Adapter<FeedAdapter.FeedViewHolder>() {
     // Táto metóda prepojí dáta s ViewHolderom
     override fun onBindViewHolder(holder: FeedViewHolder, position: Int) {
         holder.itemView.findViewById<TextView>(R.id.item_text).text = items[position].name
+
+        // Declaring and initializing the ImageView
+        val imageView = holder.itemView.findViewById<ImageView>(R.id.item_image)
+
+        // Declaring executor to parse the URL
+        val executor = Executors.newSingleThreadExecutor()
+
+        // Once the executor parses the URL
+        // and receives the image, handler will load it
+        // in the ImageView
+        val handler = Handler(Looper.getMainLooper())
+
+        // Initializing the image
+        var image: Bitmap? = null
+
+        // Only for Background process (can take time depending on the Internet speed)
+        executor.execute {
+
+            // Image URL
+            val imageURL = "https://upload.mcomputing.eu/" + items[position].photo
+
+            // Tries to get the image and post it in the ImageView
+            // with the help of Handler
+            try {
+                val `in` = java.net.URL(imageURL).openStream()
+                image = BitmapFactory.decodeStream(`in`)
+
+                // Only for making changes in UI
+                handler.post {
+                    imageView.setImageBitmap(image)
+                }
+            }
+
+            // If the URL doesnot point to
+            // image or any other kind of failure
+            catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+
+//            holder.itemView.findViewById<ImageView>(R.id.item_image). //= items[position].photo
+        }
     }
 
     // Vracia počet položiek v zozname
